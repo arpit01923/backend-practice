1941c7733bc3781fb7ca6fe6aef2dbe16c8de76c
Arpit <arpitkumar01923@gmail.com>
2025-04-14 23:15:21 +0530

    update login validations

--- src/users/dto/login-user.dto.ts
+++ src/users/dto/login-user.dto.ts
@@ -1,12 +1,20 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength, IsNotEmpty, IsDefined, Matches } from 'class-validator';
 
 export class LoginUserDto {
+    @IsDefined()
     @IsEmail()
     @IsNotEmpty()
+    @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+        message: 'Please provide a valid email address'
+    })
     email: string;
 
+    @IsDefined()
     @IsString()
+    @MinLength(8)
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+        message: 'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character'
+    })
     @IsNotEmpty()
-    @MinLength(6)
     password: string;
 } 
